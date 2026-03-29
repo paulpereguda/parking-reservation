5cabf2f0cb604475664c8b26f5b17972ea8fe438
Pavel P <pavel.p@smartym.pro>
2026-03-29 21:13:29 +0200

    init commit

--- 2/parking-reservation/src/main/java/org/CarRentalService.java
+++ 2/parking-reservation/src/main/java/org/CarRentalService.java
@@ -2,7 +2,6 @@ package org;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.locks.ReentrantLock;
